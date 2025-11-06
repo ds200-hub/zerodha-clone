f5d16b6e5462d47b0d6ca087e8b271cd6448662e
deepak <ds094p12@gmail.com>
2025-11-06 22:06:19 +0530

    udate

--- UserAuthentication/my-client/src/App.jsx
+++ UserAuthentication/my-client/src/App.jsx
@@ -1,9 +1,6 @@
 import {Route, Routes} from "react-router-dom";
-import Home from "../src/pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
-
 
 
 function App() {
