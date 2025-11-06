5866895131707ca29559ad2bb777fe56761b48fd
deepak <ds094p12@gmail.com>
2025-11-06 23:06:43 +0530

    edit4

--- UserAuthentication/my-client/src/App.jsx
+++ UserAuthentication/my-client/src/App.jsx
@@ -1,14 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 
 function App() {
   return ( 
     <>
     <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
+      {/* <Route path="/" element={<Home></Home>}></Route> */}
       <Route path="/login" element={<Login></Login>}></Route>
       <Route path="/signup" element={<Signup></Signup>}></Route>
     </Routes>
