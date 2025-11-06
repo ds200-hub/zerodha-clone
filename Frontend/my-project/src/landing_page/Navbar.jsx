0952d3a0786f7bd3cd7832bf415c7917fd7ea153
deepak <ds094p12@gmail.com>
2025-11-06 22:51:37 +0530

    update

--- Frontend/my-project/src/landing_page/Navbar.jsx
+++ Frontend/my-project/src/landing_page/Navbar.jsx
@@ -15,21 +15,10 @@ function Navbar() {
 
                     <form class="d-flex" role="search">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-
-                            
                             <li class="nav-item">
                                 {/* <Link class="nav-link active" aria-current="page" to="/Signup">SignUp</Link> */}
-                                {/* <a className='nav-link active' href="https://zerodha-authentication.vercel.app/login" target='_self'>Login</a>  */}
-                                {/*_self se hum same tab mein redirect honge aur _blank likhne se dusre tab mein redirect honge */}
+                                <a className='nav-link active' href="https://zerodha-authentication.vercel.app/login" target='_self'>Login</a> {/*_self se hum same tab mein redirect honge aur _blank likhne se dusre tab mein redirect honge */}
                             </li>
-                             <li class="nav-item">
-                                <Link class="nav-link active" to="/Login">Login</Link>
-                            </li>
-
-
-
-
                             <li class="nav-item">
                                 <Link class="nav-link active" to="/About">About</Link>
                             </li>
--- UserAuthentication/my-client/src/App.jsx
+++ UserAuthentication/my-client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return ( 
     <>
     <Routes>
+      <Route path="/" element={<Login></Login>}></Route>
       <Route path="/login" element={<Login></Login>}></Route>
       <Route path="/signup" element={<Signup></Signup>}></Route>
     </Routes>
