f5c5204b3e3d99a89d94f3e99ee6b07ac396e702
deepak <ds094p12@gmail.com>
2025-11-07 19:05:13 +0530

    dd

--- Dashboard/my-dashboard/src/main.jsx
+++ Dashboard/my-dashboard/src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <CookiesProvider>
         <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/*' element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path='/signup' element={<Signup></Signup>}></Route>
         </Routes>
