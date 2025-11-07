81b8055353080da284fe3377a9a3ede548e607b7
deepak <ds094p12@gmail.com>
2025-11-07 21:20:36 +0530

    another

--- Dashboard/my-dashboard/src/main.jsx
+++ Dashboard/my-dashboard/src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { CookiesProvider } from 'react-cookie';
-import './index.css'
-import Home from './Components/Home.jsx'
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter, Route, Routes } from "react-router-dom"
+// import { CookiesProvider } from 'react-cookie';
+// import './index.css'
+// import Home from './Components/Home.jsx'
+// import Login from './pages/Login.jsx';
+// import Signup from './pages/Signup.jsx';
 
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <CookiesProvider>
-        <Routes>
-          <Route path='/*' element={<Home></Home>}></Route>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path='/signup' element={<Signup></Signup>}></Route>
-        </Routes>
-      </CookiesProvider>
-    </BrowserRouter>
-  </StrictMode>
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <CookiesProvider>
+//         <Routes>
+//           <Route path='/*' element={<Home></Home>}></Route>
+//           <Route path='/' element={<Home></Home>}></Route>
+//           <Route path="/login" element={<Login></Login>}></Route>
+//           <Route path='/signup' element={<Signup></Signup>}></Route>
+//         </Routes>
+//       </CookiesProvider>
+//     </BrowserRouter>
+//   </StrictMode>
+// )
